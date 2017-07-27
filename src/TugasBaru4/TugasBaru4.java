8cd54eba2945d0f944bd1731984553033c3d9959
regyma <regymahmudazis@gmail.com>
2017-07-27 17:06:25 +0700

    Tugas 3 dan 4 Beserta Langkahnya

---
+++ src/TugasBaru4/TugasBaru4.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TugasBaru4;
+
+/**
+ *
+ * @author Regyma
+ */
+public class TugasBaru4 {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        MainFrm Data = new MainFrm();
+        Data.setVisible(true);
+    }
+}
